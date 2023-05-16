2ffe0cc100801ca50e2ad24238946be74f300a48
Chris Tisdale <christopher.tisdale@aristocrat.com>
2023-05-16 13:46:55 -0500

    Fixing repo URL

--- version_stamp/version_stamp.cs
+++ version_stamp/version_stamp.cs
@@ -98,7 +98,7 @@ public static class gen
             f.WriteElementString("PackageLicenseExpression", "Apache-2.0");
             f.WriteElementString("PackageRequireLicenseAcceptance", "false");
             f.WriteElementString("PackageTags", PACKAGE_TAGS);
-            f.WriteElementString("RepositoryUrl", "https://github.com/ericsink/SQLitePCL.raw");
+            f.WriteElementString("RepositoryUrl", "https://github.com/Aristocrat-Monaco-Platform/SQLitePCL.raw");
             f.WriteElementString("RepositoryType", "git");
             f.WriteElementString("PackageOutputPath", string.Format("$([System.IO.Path]::Combine($(MSBuildThisFileDirectory), '{0}'))", nupkgs_dir_name));
 
